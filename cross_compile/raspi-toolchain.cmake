b01e977b8d10a09312a8c1881e55e193fffbbdd6
Matthew Elwin <elwin@northwestern.edu>
2023-01-30 23:45:59 -0600

    Cross compile for humble almost working

--- cross_compile/raspi-toolchain.cmake
+++ cross_compile/raspi-toolchain.cmake
@@ -1,12 +1,12 @@
 cmake_minimum_required(VERSION 3.0)
 
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc CACHE STRING "The C compiler")
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc CACHE STRING "The C compiler")
 
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++ CACHE STRING "The C++ compiler") 
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++ CACHE STRING "The C++ compiler") 
 
 # only find programs on the host
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
